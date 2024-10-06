c2b3e9b6ad1830b8ca622164a93fc40f5b9cb94f
Martynenko Uriy <99603403+umartynenko@users.noreply.github.com>
2024-10-06 17:12:12 +0300

    007 Learn how to create List View and retrieve data using ForEach loop

---
+++ Fructus/View/FruitRowView.swift
@@ -0,0 +1,18 @@
+//
+//  FruitRowView.swift
+//  Fructus
+//
+//  Created by Uriy Martynenko on 06.10.2024.
+//
+
+import SwiftUI
+
+struct FruitRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FruitRowView()
+}
