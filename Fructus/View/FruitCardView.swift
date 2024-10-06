cdbbe50f4cc4c8e3a328ecb09c5d53f6ef9cc2f8
Martynenko Uriy <99603403+umartynenko@users.noreply.github.com>
2024-10-06 14:36:34 +0300

    004 Larn new to create an Onboarding Screen using the new Page Tab View

---
+++ Fructus/View/FruitCardView.swift
@@ -0,0 +1,18 @@
+//
+//  FruitCardView.swift
+//  Fructus
+//
+//  Created by Uriy Martynenko on 06.10.2024.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FruitCardView()
+}
---
+++ Fructus/View/StartButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  StartButtonView.swift
+//  Fructus
+//
+//  Created by Uriy Martynenko on 06.10.2024.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartButtonView()
+}
