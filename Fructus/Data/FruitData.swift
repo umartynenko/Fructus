d6d00117cd1410964cc3471b9f189c78ecd7ff36
Martynenko Uriy <99603403+umartynenko@users.noreply.github.com>
2024-10-06 15:37:39 +0300

    005 Learn how to create data model and fetch data using Swift

---
+++ Fructus/Data/FruitData.swift
@@ -0,0 +1,8 @@
+//
+//  FruitData.swift
+//  Fructus
+//
+//  Created by Uriy Martynenko on 06.10.2024.
+//
+
+import Foundation
---
+++ Fructus/Model/FruitModel.swift
@@ -0,0 +1,8 @@
+//
+//  Fruit.swift
+//  Fructus
+//
+//  Created by Uriy Martynenko on 06.10.2024.
+//
+
+import Foundation
