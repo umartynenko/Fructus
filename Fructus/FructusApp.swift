ae0552e6b5aa9d59ca686f7971a0d0b872340f22
Martynenko Uriy <99603403+umartynenko@users.noreply.github.com>
2024-10-06 10:22:00 +0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Uriy Martynenko on 06.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FructusTests/FructusTests.swift
@@ -0,0 +1,17 @@
+//
+//  FructusTests.swift
+//  FructusTests
+//
+//  Created by Uriy Martynenko on 06.10.2024.
+//
+
+import Testing
+@testable import Fructus
+
+struct FructusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
